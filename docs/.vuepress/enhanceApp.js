a87fd014b0734f4110806ce3b4d6bd9125e2f44f
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2018-12-12 00:28:01 +0300

    Get rid of location

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,21 +5,20 @@ export default ({
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  const isClient = typeof window !== "undefined";
-  if (isClient) {
-    router.addRoutes([{ path: "/", redirect: "/installation/" }]);
+  router.addRoutes([{ path: "/", redirect: "/installation/" }]);
 
-    router.beforeEach((to, from, next) => {
-      if (/.htm[l]*$/.test(to.fullPath)) {
-        const newUrl = URLS[location.pathname + location.search];
-        if (newUrl) {
-          router.push(newUrl);
-        } else {
-          next();
-        }
+  router.beforeEach((to, from, next) => {
+    if (/.htm[l]*$/.test(to.fullPath)) {
+      // debugger
+      const newUrl = URLS[to.fullPath];
+      if (newUrl) {
+        // debugger
+        router.push(newUrl);
       } else {
         next();
       }
-    });
-  }
+    } else {
+      next();
+    }
+  });
 };
