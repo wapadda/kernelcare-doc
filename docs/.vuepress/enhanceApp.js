7c03aed0dd882c1122ec2c3dc2f44f958183548c
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2018-12-11 23:51:08 +0300

    Added client's scope

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,18 +5,21 @@ export default ({
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  router.addRoutes([{ path: "/", redirect: "/installation/" }]);
+  const isClient = typeof window !== "undefined";
+  if (isClient) {
+    router.addRoutes([{ path: "/", redirect: "/installation/" }]);
 
-  router.beforeEach((to, from, next) => {
-    if (/.htm[l]*$/.test(to.path)) {
-      const newUrl = URLS[location.pathname + location.search];
-      if (newUrl) {
-        router.push(newUrl);
+    router.beforeEach((to, from, next) => {
+      if (/.htm[l]*$/.test(to.path)) {
+        const newUrl = URLS[location.pathname + location.search];
+        if (newUrl) {
+          router.push(newUrl);
+        } else {
+          next();
+        }
       } else {
         next();
       }
-    } else {
-      next();
-    }
-  });
+    });
+  }
 };
