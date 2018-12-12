02a31dffccef3991dac72caf5d924ff2b30d240e
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2018-12-12 12:32:34 +0300

    Mapping moved into urls-mapping.js file

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,19 +4,4 @@ export default ({
   options, // the options for the root Vue instance
   router, // the router instance for the app
   siteData // site metadata
-}) => {
-  // router.addRoutes([{ path: "/", redirect: "/installation/" }]);
-
-  // router.beforeEach((to, from, next) => {
-  //   if (/.htm[l]*$/.test(to.fullPath)) {
-  //     const newUrl = URLS[to.fullPath];
-  //     if (newUrl) {
-  //       next(newUrl)
-  //     } else {
-  //       next();
-  //     }
-  //   } else {
-  //     next();
-  //   }
-  // });
-};
+}) => {};
